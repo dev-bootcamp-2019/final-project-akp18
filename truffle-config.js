974a8d38aea0ced860cef18a983849fdcdb13254
akp18 <akp@sysguild.com>
2019-01-19 14:03:10 -0600

    added link to src/contract directory

--- truffle-config.js
+++ truffle-config.js
@@ -1,9 +1,14 @@
+const path = require("path");
+
 module.exports = {
-	networks: {
-		development: {
-		host: "localhost",
-		port: 8545,
-		network_id: "*"
-		}
-	}
+
+  contracts_build_directory: path.join(__dirname, "/client/src/contracts/"),
+
+  networks: {
+    development: {
+      host: "localhost",
+      port: 8545,
+      network_id: "*",
+    }
+  }
 };
